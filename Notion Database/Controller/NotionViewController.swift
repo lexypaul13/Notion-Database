030a401abac8419cb6a78edda2abf92b0d6e1138
Mayowa Paul <lexypaul13@yahoo.com>
2022-01-30 20:56:02 -0500

    Populated table view with the names and tags array

--- Notion Database/Controller/NotionViewController.swift
+++ Notion Database/Controller/NotionViewController.swift
@@ -8,27 +8,58 @@
 import UIKit
 
 class NotionViewController: UIViewController {
-    let networkService = NetworkService.shared
     
+    let networkService = NetworkService.shared
+    var names = [String]()
+    var tags =  [String]()
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getNameAndTags()
+        
+        setUpTableView()
+        getNamesAndTags()
     }
     
+    func setUpTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
     
-    func getNameAndTags(){
-        networkService.getJSON { results in
-            guard let results = results else {
-                return
-            }
+    func getNamesAndTags(){
+        networkService.getJSON {  [weak self ] results in
+            guard let self = self else { return}
+            guard let results = results else {return}
             for result in results {
-                let name = result.properties.name.title.map { $0.plainText }
-                let tags = result.properties.tags.multiSelect.map { $0.name }
-                print("\(name) - \(tags)")
+                self.names = result.properties.name.title.map { $0.plainText }
+                self.tags = result.properties.tags.multiSelect.map { $0.name }
+               
+                print(self.names)
+                print(self.tags)
             }
+            self.tableView.reloadData()
+
         }
     }
     
 }
-
+extension NotionViewController: UITableViewDelegate,UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? NotionTableViewCell
+        cell?.nameLabel.text = names[indexPath.row]
+        cell?.tagLabel.text = tags[indexPath.row]
+        return cell!
+    }
+    
+    
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 163
+    }
+    
+}
